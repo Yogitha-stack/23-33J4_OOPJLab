2c9caff508267f3505fb03bc64bb958fb416ce4e
Yogitha-stack <yogithamukthapuram2006@gmail.com>
2025-01-29 20:51:21 +0530

    Add files via upload

---
+++ simpleclass.java
@@ -0,0 +1,15 @@
+ public class simpleclass
+{
+public  void printmessage()
+{
+System.out.println("hello,welcome to csm");
+}
+public static void main(String args[])
+{
+simpleclass obj=new  simpleclass();
+obj.printmessage();
+}
+}
+
+OUTPUT
+hello,welcome to csm
